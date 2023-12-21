f4d959c1dcb3e24946735b0f69d3a3b8bc8a2b6b
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-12-21 13:12:51 +0200

    cleaning-comments

--- routes/auth-routers.js
+++ routes/auth-routers.js
@@ -7,11 +7,8 @@ import { authenticate } from "../middlewares/index.js";
 const authRouter = express.Router();
 const userSignupValidate = validateBody(userSchemas.userSignupSchema);
 const userSigninValidate = validateBody(userSchemas.userSigninSchema);
-const userRefreshValidate = validateBody(userSchemas.userVerifySchema);
 
 authRouter.post("/register", userSignupValidate, authControler.signup);
-authRouter.get("/verify/:verificationToken", authControler.verifyEmail);
-authRouter.post("/verify", userRefreshValidate, authControler.resendVerify);
 authRouter.post("/login", userSigninValidate, authControler.signin);
 authRouter.get("/current", authenticate, authControler.getCurrent);
 authRouter.post("/logout", authenticate, authControler.signout);
