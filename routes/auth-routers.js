b742778a2437c3042bd05fc6dde78a8ef50b1951
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-12-21 16:27:36 +0200

    update-model-user

--- routes/auth-routers.js
+++ routes/auth-routers.js
@@ -5,11 +5,10 @@ import authControler from "../controllers/auth-controler.js";
 import { authenticate } from "../middlewares/index.js";
 
 const authRouter = express.Router();
-const userSignupValidate = validateBody(userSchemas.userSignupSchema);
-const userSigninValidate = validateBody(userSchemas.userSigninSchema);
+const registerAndLoginSchema = validateBody(userSchemas.registerAndLoginSchema);
 
-authRouter.post("/register", userSignupValidate, authControler.signup);
-authRouter.post("/login", userSigninValidate, authControler.signin);
+authRouter.post("/register", registerAndLoginSchema, authControler.signup);
+authRouter.post("/login", registerAndLoginSchema, authControler.signin);
 authRouter.get("/current", authenticate, authControler.getCurrent);
 authRouter.post("/logout", authenticate, authControler.signout);
 
