9fe9c478def7e4aa7cbca58bc155ebea3cd7d47c
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2023-12-22 21:21:11 +0200

    Remove is emptyBody and add isValidId

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,3 +1,3 @@
 export { default as authenticate } from "./autenticate.js";
-export { default as isEmptyBody } from "./isEmptyBody.js";
 export { default as upload } from "./upload.js";
+export { default as isValidId } from "./isValidId.js";
