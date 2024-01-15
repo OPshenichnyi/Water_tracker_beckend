89f6e2b2509844928cb06c67ea9eac359ac906f1
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-01-15 21:01:33 +0200

    Add function not sleep

--- routes/auth-routers.js
+++ routes/auth-routers.js
@@ -9,14 +9,30 @@ const registerAndLoginSchema = validateBody(userSchemas.registerAndLoginSchema);
 const updateProfilSchema = validateBody(userSchemas.updateProfilSchema);
 const waterRateSchema = validateBody(userSchemas.waterRateSchema);
 
+authRouter.get("/sleep", authControler.dontSleep);
 authRouter.post("/register", registerAndLoginSchema, authControler.signup);
 authRouter.post("/login", registerAndLoginSchema, authControler.signin);
 authRouter.post("/logout", authenticate, authControler.signout);
 
-authRouter.patch("/avatars", upload.single("avatarURL"), authenticate, authControler.updateAvatar);
+authRouter.patch(
+  "/avatars",
+  upload.single("avatarURL"),
+  authenticate,
+  authControler.updateAvatar
+);
 authRouter.get("/current", authenticate, authControler.getCurrent);
-authRouter.patch("/", authenticate, updateProfilSchema, authControler.updateProfil);
+authRouter.patch(
+  "/",
+  authenticate,
+  updateProfilSchema,
+  authControler.updateProfil
+);
 
-authRouter.patch("/water-rate",authenticate, waterRateSchema, authControler.waterRate);
+authRouter.patch(
+  "/water-rate",
+  authenticate,
+  waterRateSchema,
+  authControler.waterRate
+);
 
 export default authRouter;
